18598ac5213da90756d8ec2c8f5f3d0aa1dbb761
MaksFlyOk <130773305+MaksFlyOk@users.noreply.github.com>
2025-08-27 23:09:08 +0700

    Added logic for processing parameters in main

--- src/main/java/ru/shift/utility/FileContentFilterUtility.java
+++ src/main/java/ru/shift/utility/FileContentFilterUtility.java
@@ -1,7 +1,22 @@
 package ru.shift.utility;
 
+import org.apache.commons.cli.*;
+import ru.shift.utility.cli.Params;
+
+import static ru.shift.utility.cli.ArgumentParser.parseArgs;
+
 public class FileContentFilterUtility {
   public static void main(String[] args) {
+    try {
+      Params params = parseArgs(args);
 
+    } catch (ParseException e) {
+      System.err.println("Error: " + e.getMessage());
+      System.exit(2);
+    } catch (Exception e) {
+      System.err.println("Unexpected error occurred: " + e.getMessage());
+      e.printStackTrace();
+      System.exit(3);
+    }
   }
 }

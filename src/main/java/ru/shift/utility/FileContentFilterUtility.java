364e19276ba85e5d2405ebb38943ac4669c4f0c6
MaksFlyOk <130773305+MaksFlyOk@users.noreply.github.com>
2025-09-01 23:49:40 +0700

    Update class FileContentFilterUtility main method

--- src/main/java/ru/shift/utility/FileContentFilterUtility.java
+++ src/main/java/ru/shift/utility/FileContentFilterUtility.java
@@ -2,6 +2,14 @@ package ru.shift.utility;
 
 import org.apache.commons.cli.*;
 import ru.shift.utility.cli.Params;
+import ru.shift.utility.core.FilesProcessor;
+import ru.shift.utility.core.FilesWriter;
+import ru.shift.utility.core.LineClassifier;
+import ru.shift.utility.core.LineType;
+import ru.shift.utility.statistic.FullStatistics;
+import ru.shift.utility.statistic.Statistics;
+
+import java.util.List;
 
 import static ru.shift.utility.cli.ArgumentParser.parseArgs;
 
@@ -10,12 +18,23 @@ public class FileContentFilterUtility {
     try {
       Params params = parseArgs(args);
 
+      final List<String> handledFiles = FilesProcessor.fileHandling(params.files());
+
+      FilesWriter filesWriter = new FilesWriter(params.filePrefix(), params.writeFilesPath(), params.aEnabled());
+      Statistics statistic = params.fEnabled() ? new FullStatistics() : new Statistics();
+
+      for (String line : handledFiles) {
+        LineType lineType = LineClassifier.getType(line);
+
+        filesWriter.lineWrite(line, lineType.toString().toLowerCase());
+        statistic.processLine(lineType, line);
+      }
+
+      if (params.sEnabled() || params.fEnabled()) statistic.printStatistics();
     } catch (ParseException e) {
-      System.err.println("Error: " + e.getMessage());
       System.exit(2);
     } catch (Exception e) {
       System.err.println("Unexpected error occurred: " + e.getMessage());
-      e.printStackTrace();
       System.exit(3);
     }
   }
